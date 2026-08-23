f922e844e44f87127fa930bd6941499b4afa691c
Link4Electronics <link4electronics@gmail.com>
2026-08-23 08:55:36 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,13 +6,48 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm tinyxml2
+pacman -Syu --noconfirm \
+    cmake         \
+    fmt           \
+    libdecor      \
+    libzip        \
+    ninja         \
+    nlohmann-json \
+    sdl2          \
+    spdlog        \
+    tcc           \
+    tinyxml2
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-make-aur-package
+make-aur-package zenity-rs-bin
 
-# If the application needs to be manually built that has to be done down here
+echo "Making stable build of Starship..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/HarbourMasters/Starship"
+VERSION="$(git ls-remote --tags --sort="v:refname" "$REPO" | tail -n1 | sed 's/.*\///; s/\^{}//')"
+git clone --branch "$VERSION" --single-branch --recursive --depth 1 "$REPO" ./Ghostship
+echo "$VERSION" > ~/version
+
+mkdir -p ./AppDir/bin
+cd ./Ghostship
+patch -Np1 -i "../starship-stack-underflow-guard.patch"
+
+cmake . \
+    -Bbuild \
+    -GNinja \
+    -DCMAKE_POSITION_INDEPENDENT_CODE=ON
+
+cmake --build build --config Release
+cmake --build build --config Release --target GeneratePortO2R
+
+mv -v build/assets ../AppDir/bin
+mv -v build/Ghostship ../AppDir/bin
+mv -v build/config.yml ../AppDir/bin
+mv -v build/ghostship.o2r ../AppDir/bin
+mv -v libultraship/libtcc.so ../AppDir/bin
+wget -O ../AppDir/bin/gamecontrollerdb.txt https://raw.githubusercontent.com/mdqinc/SDL_GameControllerDB/master/gamecontrollerdb.txt
+cp -v logo.png ../AppDir/.DirIcon
+mv -v logo.png ../AppDir/ghostship.png
